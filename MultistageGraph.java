be28d978cf589e8f54870b0d1d35405bf811c068
Ashish Goel <ashish84goel@gmail.com>
2021-09-24 23:30:22 +0530

    Update MultistageGraph.java

--- MultistageGraph.java
+++ MultistageGraph.java
@@ -15,23 +15,24 @@ Multistage Graph Theory: https://www.youtube.com/watch?v=9iE9Mj4m8jk
 public class MultistageGraph
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+	    System.out.println("Hello World");
+	    int n = 4;
 		
-		Integer[][] arr= {
+	    Integer[][] arr= {
 		    { null, 3,       2, null },
 		    { null, null, null,    4 },
 		    { null, null, null,    6 },
 		    { null, null, null, null }
 	    };
 	    
-	    Integer cost[] = new Integer[4];
-	    Integer dest[] = new Integer[4];
+	    Integer cost[] = new Integer[n];
+	    Integer dest[] = new Integer[n];
 	    
-	    cost[3] = 0;
-	    dest[3] = 3;
+	    cost[n-1] = 0;
+	    dest[n-1] = n-1;
 	    
-	    for (int i=2; i>=0; i--) {
-	        for (int j=i+1; j<=3; j++) {
+	    for (int i=n-1; i>=0; i--) {
+	        for (int j=i+1; j<=n-1; j++) {
 	            if (arr[i][j] != null) {
 	                if (cost[i] == null) {
 	                    cost[i] = arr[i][j] + cost[j];
@@ -46,13 +47,14 @@ public class MultistageGraph
 	        }
 	    }
 		    
-		for (int k = 0; k<4; k++) {
+		for (int k = 0; k<n; k++) {
 		    System.out.println("node = " + k + " --> cost = " + cost[k] + " dest = " + dest[k]);
 		}
 		
 		System.out.println("Finding shortest path...");
 		System.out.print("0");
-		for (int p=0; p<4; p++) {
+		int p=0;
+		while (p<n-1) {
 		    System.out.print("-->" + dest[p]);
 		    p = dest[p];
 		}
